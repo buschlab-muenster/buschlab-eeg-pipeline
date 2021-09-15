f474fbcf30865e155218d68f395b1d94e7df38fc
buschlab-muenster <90451818+buschlab-muenster@users.noreply.github.com>
2021-09-15 18:09:08 +0200

    Adapted function to work with PARFOR loop ==> much faster now!

--- code/design_functions/ed_grandaverage.m
+++ code/design_functions/ed_grandaverage.m
@@ -6,63 +6,84 @@ G.data_type = data_type;
 G.DINFO = get_design_matrix(D);
 [G.data] = deal(cell(G.DINFO.nlevels+1)); % +1 because we add "all levels together" as another level.
 G.ntrials = cell(G.DINFO.nlevels+1); % +1 because we add "all levels together" as another level.
-cond_average = [];
 
-for isub = 1:length(subjects)
+n_conditions = length(G.DINFO.design_matrix);
+n_subjects = length(subjects);
+n_trials = cell(n_conditions, n_subjects);
+subject_names = cell(1, n_subjects);
+
+% Load one EEG set just for the meta data and for initializing the data
+% matrix.
+eeg_meta_data = pop_loadset('filename', subjects(1).name, ...
+    'filepath', subjects(1).folder, 'loadmode', 'info');
+
+cond_average = cell(n_conditions, n_subjects);
+[cond_average{:,:}] = deal(nan(eeg_meta_data.nbchan, eeg_meta_data.pnts));
+
+% Store meta data because we cannot do that within a parfor loop
+switch data_type    
+    case 'erp'
+        G.times = eeg_meta_data.times;
+        G.chanlocs = eeg_meta_data.chanlocs;    
+    case 'filtbert'
+        G.filtbert_fband = eeg_meta_data.filtbert_fband;
+        G.times = eeg_meta_data.times;
+        G.chanlocs = eeg_meta_data.chanlocs;        
+    case 'fft'
+        G.fft_twin = eeg_meta_data.fft_twin;
+        G.fft_freqs = eeg_meta_data.fft_freqs;
+        G.chanlocs = eeg_meta_data.chanlocs;
+end
+
+%%
+parfor isub = 1:length(subjects)
     
     % --------------------------------------------------------
     % Load data and extract relevant meta-info, depending on data_type
-    switch data_type
-            
+    switch data_type        
         case 'erp'
             EEG = pop_loadset('filename', subjects(isub).name, ...
                 'filepath', subjects(isub).folder);
             subject_data = EEG.data;
-            events = EEG.event; 
-            G.times = EEG.times;
-            G.chanlocs = EEG.chanlocs;
+            events = EEG.event;
             
         case 'filtbert'
             EEG = pop_loadset('filename', subjects(isub).name, ...
                 'filepath', subjects(isub).folder);
-            subject_data = EEG.data;     
-            events = EEG.event;        
-            G.filtbert_fband = EEG.filtbert_fband;
-            G.times = EEG.times;
-            G.chanlocs = EEG.chanlocs;
+            subject_data = EEG.data;
             events = EEG.event;
             
         case 'fft'
             EEG = pop_loadset('filename', subjects(isub).name, ...
                 'filepath', subjects(isub).folder);
             subject_data = EEG.fft_amps;
-            events = EEG.event; 
-            G.fft_twin = EEG.fft_twin;
-            G.fft_freqs = EEG.fft_freqs;    
-            G.chanlocs = EEG.chanlocs;  
-            
+            events = EEG.event;            
     end
     
-    G.subjects{isub} = subjects(isub).name;
+    subject_names{isub} = subjects(isub).name;
     
     % Extract relevant trials for each condition.
+    fprintf('Extracting relevant trials.\n')
+
     design_trials = get_design_trials(events, G.DINFO);
     
-    for icond = 1:length(G.DINFO.design_matrix)    
-        
-        ntrials{icond, isub} = length(design_trials(icond).trials);  
+    for icond = 1:n_conditions        
+        n_trials{icond, isub} = length(design_trials(icond).trials);
         
         cond_data = subject_data(:,:,design_trials(icond).trials);
-        cond_average{icond}{isub} = mean(cond_data, 3);              
+        cond_average{icond, isub} = mean(cond_data, 3);
     end
     
 end
 
-%%
-for icond = 1:length(G.DINFO.design_matrix)    
-    G.data{icond}       = cat(3,cond_average{icond}{:});
-%     G.fft_amps{icond}  = cat(3,fft_amps{icond}{:});
-%     G.fft_freqs{icond} = cat(3,fft_freqs{icond}{:});
-    G.ntrials{icond}   = ntrials(icond,:);
+%% 
+fprintf('Accumulating subject data.\n')
+% Now that the parfor loop is finished, we can process a few more things
+% that cannot be resolved in parfor.
+G.subjects = subject_names;
+
+for icond = 1:length(G.DINFO.design_matrix)
+    G.data{icond}       = cat(3,cond_average{icond,:});
+    G.ntrials{icond}   = n_trials(icond,:);
 end
 
