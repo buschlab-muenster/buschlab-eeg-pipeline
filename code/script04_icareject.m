889a446454f59329f162716d4c1473914ae3ee99
Niko <n.ikobusch@gmail.com>
2021-09-22 16:46:43 +0200

    Modified to allow fully automatic ICA rejection

--- code/script04_icareject.m
+++ code/script04_icareject.m
@@ -27,56 +27,94 @@ for i = 1:length(icareject_fields)
 end
 
 %% Run across subjects.
-for isub = 1:length(subjects)
+nthreads = min([prefs.max_threads, length(subjects)]);
+parfor(isub = 1:length(subjects), nthreads) % Use this if you do NOT use manual confirmation
+% for isub = 1%:length(subjects) % Use this if you want to manually inspect ICs.
     
     
     % --------------------------------------------------------------
     % Load the dataset and initialize the list of bad ICs.
-    % --------------------------------------------------------------    
-    [ALLEEG EEG CURRENTSET ALLCOM] = eeglab;
-    EEG = pop_loadset('filename', subjects(isub).name, 'filepath', subjects(isub).folder);
-    [ALLEEG, EEG, CURRENTSET] = eeg_store( ALLEEG, EEG, 0 );
+    % --------------------------------------------------------------
+    if cfg.icareject.confirm_manual
+        [ALLEEG EEG CURRENTSET ALLCOM] = eeglab;
+    end
     
-    [bad_ics_eog, bad_ics_eyetracker, bad_ics_iclabel] = deal([]);
+    EEG = pop_loadset('filename', subjects(isub).name, 'filepath', subjects(isub).folder);
+    EEG = pop_rmbase(EEG, [], []);
+    [bad_ics_eog, bad_ics_eyetracker, bad_ics_iclabel] = deal(zeros(length(EEG.reject.gcompreject), 1));
+    
+    if cfg.icareject.confirm_manual
+        [ALLEEG, EEG, CURRENTSET] = eeg_store( ALLEEG, EEG, 0 );
+    end
     
     % --------------------------------------------------------------
     % Reject ICs that correlate with HEOG/VEOG..
     % --------------------------------------------------------------
-    %     if cfg.icareject.do_correlate_eog==true
-    %         [bad_ics_eog, ~] = func_icareject_corr_ic_eog(EEG, ...
-    %             [cfg.chans.HEOGchan cfg.chans.VEOGchan], ...
-    %             cfg.icareject.thresh_correlate_eog);
-    %     end
+    if cfg.icareject.do_correlate_eog==true
+        fprintf('Detecting ICs that correlate with EOG channels > %2.2f\n', ...
+            cfg.icareject.thresh_correlate_eog)
+        
+        [bad_ics_eog, ~] = func_icareject_corr_ic_eog(EEG, ...
+            [cfg.chans.HEOGchan cfg.chans.VEOGchan], ...
+            cfg.icareject.thresh_correlate_eog);
+    end
     
     % --------------------------------------------------------------
     % Reject ICs that correlate with eye tracker.
     % IMPORTANT: you can only use this if the data are not resampled,
     % see documentation of pop_eyetrackerica!
     % --------------------------------------------------------------
-    % if cfg.icareject.do_eyetrackerica==true && cfg.prep.do_resampling==false
-    %     [bad_ics_eyetracker] = func_icareject_eyetrackerica(EEG, cfg.icareject);
-    % end
+    if cfg.icareject.do_eyetrackerica==true && cfg.prep.do_resampling==false
+        fprintf('Detecting ICs that correlate with eye tracker.\n')
+        [bad_ics_eyetracker] = func_icareject_eyetrackerica(EEG, cfg.icareject);
+    end
+    
     
     % --------------------------------------------------------------
     % Detect bad ICs with IC label.
     % --------------------------------------------------------------
     if cfg.icareject.do_iclabel==true
-        EEG = func_icareject_iclabel(EEG, cfg.icareject, bad_ics_eog);
+        fprintf('Detecting ICs with IClabel.\n')
+        [EEG, bad_ics_iclabel] = func_icareject_iclabel(EEG, cfg.icareject, bad_ics_eog);
     end
+    
+    
+    % --------------------------------------------------------------
+    % Now that all detection procedures are finished, update the list 
+    % of bad ICs. This is important so that the manual inspection shows 
+    % bad ICs flagged by any of the procedures.
+    % --------------------------------------------------------------
+    flag_ics = [bad_ics_iclabel | bad_ics_eog | bad_ics_eyetracker];
+    EEG.reject.gcompreject = flag_ics;
+    
     
     % --------------------------------------------------------------
     % Manual inspection.
     % --------------------------------------------------------------
-    EEG = func_icareject_manualinspect(EEG);
+    if cfg.icareject.confirm_manual
+        [ALLEEG EEG CURRENTSET] = eeg_store(ALLEEG, EEG, CURRENTSET);
+        EEG = func_icareject_manualinspect(EEG);
+    end
+    
     
     % --------------------------------------------------------------
     % Finally subtract all bad components.
     % --------------------------------------------------------------
-    EEG = pop_subcomp(EEG, [], 1);
+    remove_ics = find(EEG.reject.gcompreject);
+    fprintf('Removing %d components:\n', length(remove_ics))
+    fprintf(' %g', remove_ics)
+    fprintf('.\n')
+    
+    if cfg.icareject.confirm_manual        
+        EEG = pop_subcomp(EEG, [], 1);
+    else
+        EEG = pop_subcomp(EEG, remove_ics, 0);
+    end
+        
     
     % --------------------------------------------------------------
     % Save clean data.
-    % --------------------------------------------------------------    
+    % --------------------------------------------------------------
     EEG = func_saveset(EEG, subjects(isub));
 
 end
